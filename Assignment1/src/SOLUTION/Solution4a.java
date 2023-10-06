d9313d950e9316d3d7a4e89d14ef399048a0771c
Prernagupta31 <prerna3151996@gmail.com>
2023-10-07 00:08:13 +0530

    Solution 4 is uploaded

--- Assignment1/src/SOLUTION/Solution4a.java
+++ Assignment1/src/SOLUTION/Solution4a.java
@@ -39,8 +39,57 @@ public class Solution4a {
 			}
 		System.out.println();
 	}
-     }
+		System.out.println("*************PART b**************");
+		/*for(int i=1;i<=9;i++) {
+			for(int j=1;j<9-i;j++) {
+				System.out.println(" ");
+			}
+			for(int k=1;k<=i;k++) {
+				System.out.println(" *");
+			}
+			System.out.println(); 
+		}*/
+		for(int i=1;i<6;i++) {
+			for(int j=1; j<=6-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=1; k<=i; k++) {
+				System.out.print(" *");
+			
+			}
+			System.out.println();
+			
 	}
+		for(int i=5;i>0;i--) {
+			for(int j=1; j<=6-i; j++) {
+				System.out.print(" ");
+			}
+			for(int k=i; k<=2*i-1; k++) {
+				System.out.print(" *");
+			
+			}
+			System.out.println();
+			
+	
+			}
+		System.out.println("*************PART C**************");
+		for (int i = 1; i <= 5; i++) {
+			for (int k = 5 - 1; k >=i; k--) {
+				System.out.print(" ");
+			}
+
+			for (int j = 0; j < 2*i-1; j++) {
+
+				System.out.print("*");
+			}
+
+			System.out.println();
+		}
+	}
+		
+	}
+	
+	
 
 
 	
