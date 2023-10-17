bbea7c0d4075ca4cac9bfcb08fa56f0cd134818d
Prernagupta31 <prerna3151996@gmail.com>
2023-10-17 22:18:02 +0530

    Question 4 of Assignment9 is uploaded

--- Assignment9/Demo/src/com/book/Book.java
+++ Assignment9/Demo/src/com/book/Book.java
@@ -6,6 +6,16 @@ private String isbn;
 private double price;
 private String authorName;
 private int qty;
+public Book() {
+	this("",0.0,"",0);
+}
+public Book(String isbn, double price, String authorName, int qty) {
+	
+	this.isbn = isbn;
+	this.price = price;
+	this.authorName = authorName;
+	this.qty = qty;
+}
 public String getIsbn() {
 	return isbn;
 }
@@ -40,4 +50,21 @@ public void acceptBook() {
 	System.out.println("Enter quantity");
 	qty=sc.nextInt();
 }
+
+@Override
+public String toString() {
+	return "Book [isbn=" + isbn + ", price=" + price + ", authorName=" + authorName + ", qty=" + qty + "]";
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	Book other = (Book) obj;
+	if(other.getIsbn().equals(this.isbn))
+		return true;
+	return false;
+}
+
 }
--- Assignment9/Demo/src/com/book/Testerbook.java
+++ Assignment9/Demo/src/com/book/Testerbook.java
@@ -7,7 +7,7 @@ private static final int case1 = 0;
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	int choice=1;
-	
+	List<Book> list=new ArrayList<>();
 		do {
 			System.out.println("****************************");
 			System.out.println("______MENU_______");
@@ -24,8 +24,43 @@ public static void main(String[] args) {
 			System.out.println("****************************");
 			
 			switch(choice){
-			case1 : List<Book> list=new ArrayList<>();
-			          
+			case 1 : Book book=new Book();
+			        book.acceptBook();
+			        list.add(book);
+			    break;
+			case 2: for(Book b:list)
+				      System.out.println(b);
+			    System.out.println();
+			    break;
+			case 3: int index;
+			      System.out.println("Enter index at which u want to remove: ");
+			      index=sc.nextInt();
+				list.remove(index);
+				break;
+			case 4: System.out.println("Enter isbn: ");	
+				String x=sc.next();
+				Book key=new Book();
+				key.setIsbn(x);
+				System.out.println(list.contains(key));
+			          break;
+			case 5: list.clear();
+			break;
+			case 6: System.out.println("number of books: "+list.size());
+			break;
+			case 7: class Arrsort implements Comparator<Book>{
+
+				@Override
+				public int compare(Book b1, Book b2) {
+					// TODO Auto-generated method stub
+					int diff=Double.compare(b1.getPrice(),b2.getPrice());
+					return diff;
+					
+				}
+				
+			}
+			Arrsort arr=new Arrsort();
+			list.sort(arr);
+			
 			         
 			}
 		}while(choice!=0);
